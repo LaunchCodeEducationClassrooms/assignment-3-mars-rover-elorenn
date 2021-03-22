81e05192b258c6bc507222a4167ef0d18fd5694c
Lorena Sarasua <lorena.sarasua@gmail.com>
2021-03-22 04:14:22 +0000

    Parts 1 & 2

--- message.js
+++ message.js
@@ -1,5 +1,16 @@
+// name is a string that is the name of the message.
+// commands is an array of Command objects.
+// throws error if a name is NOT passed into the constructor as the first parameter
+
 class Message {
-   // Write code here!
+  // Write code here!
+  constructor (name, commands) {
+    this.name = name;
+    if (!name) {
+      throw Error('Message name required.');
+    }
+    this.commands = commands;
+  }
 }
 
 module.exports = Message;
